637c395f0ee9ea033474e33e24b65c6f884e8e3f
Andreas Adlichhammer <xyzapip@gmail.com>
2024-01-17 10:46:58 +0100

    removed base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aahhammer.github.io',
-  base: '/astro_test',
   integrations: [mdx(), sitemap(), tailwind()]
 });
