925da53c71e96a9598e86384baefdce9336d4083
Andreas Adlichhammer <xyzapip@gmail.com>
2024-01-17 10:45:03 +0100

    clean out

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://aahhammer.github.io',
+  base: '/astro_test',
   integrations: [mdx(), sitemap(), tailwind()]
 });
